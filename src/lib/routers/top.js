8251697286ce573fb9f7406b83935400bcbcfc45
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-01-13 10:18:46 -0800

    added version route

--- src/lib/routers/top.js
+++ src/lib/routers/top.js
@@ -2,7 +2,13 @@ module.exports = ({router, app, logger, context}) => {
 
     router.get('/', function (req, res) {
         res.send({});
-    })
+    });
+
+    router.get('/version', function (req, res) {
+        res.send({
+            version: require('../../version')
+        });
+    });
 
     app.use('/', router);
 };
