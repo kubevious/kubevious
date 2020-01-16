18d99877885e3149bb90d6821258476270eb06a9
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-01-15 22:26:34 -0800

    added image support

--- src/lib/logic/parsers/030_app-controllers.js
+++ src/lib/logic/parsers/030_app-controllers.js
@@ -66,100 +66,9 @@ module.exports = {
         var containersMap = _.makeDict(containersConfig, x => x.name);
 
         if (_.isArray(containersConfig)) {
-            for(var containerConfig of containersConfig) {
-                var container = app.fetchByNaming("cont", containerConfig.name);
-                scope.setK8sConfig(container, containerConfig);
-
-                var envVars = {
-                }
-
-                if (containerConfig.env) {
-                    for(var envObj of containerConfig.env) {
-                        var value = null;
-                        if (envObj.value) {
-                            value = envObj.value;
-                        } else if (envObj.valueFrom) {
-                            value = "<pre>" + yaml.safeDump(envObj.valueFrom) + "</pre>";
-                        }
-                        envVars[envObj.name] = value;
-                    }
-                }
-
-                if (containerConfig.envFrom) {
-                    for(var envFromObj of containerConfig.envFrom) {
-                        if (envFromObj.configMapRef) {
-                            var configMapScope = findAndProcessConfigMap(container, envFromObj.configMapRef.name, true);
-                            if (configMapScope) {
-                                if (configMapScope.config.data) {
-                                    for(var dataKey of _.keys(configMapScope.config.data)) {
-                                        envVars[dataKey] = configMapScope.config.data[dataKey];
-                                    }
-                                } else {
-                                    container.addAlert("EmptyConfig", "warn", null, 'ConfigMap has no data: ' + envFromObj.configMapRef.name);
-                                }
-                            }
-                        }
-                        var value = null;
-                        if (envObj.value) {
-                            value = envObj.value;
-                        } else if (envObj.valueFrom) {
-                            value = "<pre>" + yaml.safeDump(envObj.valueFrom) + "</pre>";
-                        }
-                        envVars[envObj.name] = value;
-                    }
-                }
-
-
-                if (_.keys(envVars).length > 0) {
-                    container.addProperties({
-                        kind: "key-value",
-                        id: "env",
-                        title: "Environment Variables",
-                        order: 10,
-                        config: envVars
-                    });    
-                }
-
-                if (_.isArray(containerConfig.volumeMounts)) {
-                    for(var volumeRefConfig of containerConfig.volumeMounts) {
-                        var volumeConfig = volumesMap[volumeRefConfig.name];
-                        if (volumeConfig) {
-                            var volumeItem = processVolumeConfig(
-                                container, 
-                                volumeConfig,
-                                true);
-
-                            volumeItem.addProperties({
-                                kind: "yaml",
-                                id: "env",
-                                title: "Mount Config",
-                                order: 5,
-                                config: volumeRefConfig
-                            });  
-                        }
-                    }
-                }
-
-                if (_.isArray(containerConfig.ports)) {
-                    for(var portConfig of containerConfig.ports) {
-                        var portName = portConfig.protocol + "-" + portConfig.containerPort;
-                        if (portConfig.name) {
-                            portName = portConfig.name + " (" + portName + ")";
-                        }
-                        var portItem = container.fetchByNaming("port", portName);
-                        scope.setK8sConfig(portItem, portConfig);
-
-                        var portConfigScope = {
-                            name: portConfig.name,
-                            containerName: containerConfig.name,
-                            portItem: portItem,
-                            containerItem: container
-                        };
-
-                        appScope.ports[portConfig.name] = portConfigScope;
-                        appScope.ports[portConfig.containerPort] = portConfigScope;
-                    }
-                }
+            for(var containerConfig of containersConfig)
+            {
+                processContainer(containerConfig);
             }
         }
 
@@ -191,6 +100,130 @@ module.exports = {
         });  
 
         /*** HELPERS ***/
+
+        function processContainer(containerConfig)
+        {
+            var container = app.fetchByNaming("cont", containerConfig.name);
+            scope.setK8sConfig(container, containerConfig);
+
+            if (containerConfig.image) {
+                var imageName = containerConfig.image;
+                var imageTag;
+                var i = imageName.indexOf(':');
+                if (i != -1) {
+                    imageTag = imageName.substring(i + 1);
+                    imageName = imageName.substring(0, i);
+                } else {
+                    imageTag = 'latest';
+                }
+
+                var imageItem = container.fetchByNaming("image", imageName);
+                imageItem.addProperties({
+                    kind: "key-value",
+                    id: "props",
+                    title: "Properties",
+                    order: 10,
+                    config: {
+                        name: imageName,
+                        tag: imageTag,
+                        fullName: containerConfig.image
+                    }
+                });  
+
+            }
+
+            var envVars = {
+            }
+
+            if (containerConfig.env) {
+                for(var envObj of containerConfig.env) {
+                    var value = null;
+                    if (envObj.value) {
+                        value = envObj.value;
+                    } else if (envObj.valueFrom) {
+                        value = "<pre>" + yaml.safeDump(envObj.valueFrom) + "</pre>";
+                    }
+                    envVars[envObj.name] = value;
+                }
+            }
+
+            if (containerConfig.envFrom) {
+                for(var envFromObj of containerConfig.envFrom) {
+                    if (envFromObj.configMapRef) {
+                        var configMapScope = findAndProcessConfigMap(container, envFromObj.configMapRef.name, true);
+                        if (configMapScope) {
+                            if (configMapScope.config.data) {
+                                for(var dataKey of _.keys(configMapScope.config.data)) {
+                                    envVars[dataKey] = configMapScope.config.data[dataKey];
+                                }
+                            } else {
+                                container.addAlert("EmptyConfig", "warn", null, 'ConfigMap has no data: ' + envFromObj.configMapRef.name);
+                            }
+                        }
+                    }
+                    var value = null;
+                    if (envObj.value) {
+                        value = envObj.value;
+                    } else if (envObj.valueFrom) {
+                        value = "<pre>" + yaml.safeDump(envObj.valueFrom) + "</pre>";
+                    }
+                    envVars[envObj.name] = value;
+                }
+            }
+
+
+            if (_.keys(envVars).length > 0) {
+                container.addProperties({
+                    kind: "key-value",
+                    id: "env",
+                    title: "Environment Variables",
+                    order: 10,
+                    config: envVars
+                });    
+            }
+
+            if (_.isArray(containerConfig.volumeMounts)) {
+                for(var volumeRefConfig of containerConfig.volumeMounts) {
+                    var volumeConfig = volumesMap[volumeRefConfig.name];
+                    if (volumeConfig) {
+                        var volumeItem = processVolumeConfig(
+                            container, 
+                            volumeConfig,
+                            true);
+
+                        volumeItem.addProperties({
+                            kind: "yaml",
+                            id: "env",
+                            title: "Mount Config",
+                            order: 5,
+                            config: volumeRefConfig
+                        });  
+                    }
+                }
+            }
+
+            if (_.isArray(containerConfig.ports)) {
+                for(var portConfig of containerConfig.ports) {
+                    var portName = portConfig.protocol + "-" + portConfig.containerPort;
+                    if (portConfig.name) {
+                        portName = portConfig.name + " (" + portName + ")";
+                    }
+                    var portItem = container.fetchByNaming("port", portName);
+                    scope.setK8sConfig(portItem, portConfig);
+
+                    var portConfigScope = {
+                        name: portConfig.name,
+                        containerName: containerConfig.name,
+                        portItem: portItem,
+                        containerItem: container
+                    };
+
+                    appScope.ports[portConfig.name] = portConfigScope;
+                    appScope.ports[portConfig.containerPort] = portConfigScope;
+                }
+            }
+        }
+
         function processVolumeConfig(parent, volumeConfig, markUsedBy)
         {
             var volume = parent.fetchByNaming("vol", volumeConfig.name);
