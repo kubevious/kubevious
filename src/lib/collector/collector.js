d884ade0a23e152d0c42ab87286bb517ddf5557e
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-03-30 21:20:14 -0700

    basic collector implementation

--- src/lib/collector/collector.js
+++ src/lib/collector/collector.js
@@ -1,5 +1,7 @@
 const Promise = require('the-promise');
 const _ = require('lodash');
+const { v4: uuidv4 } = require('uuid');
+
 
 class Collector
 {
@@ -8,7 +10,10 @@ class Collector
         this._context = context;
         this._logger = context.logger.sublogger("Collector");
 
-        this.logger.info("[constructed] ")
+        this.logger.info("[constructed] ");
+
+        this._snapshots = {};
+        this._diffs = {};
     }
 
     get logger() {
@@ -17,40 +22,127 @@ class Collector
     
     newSnapshot(date)
     {
+        var id = uuidv4();
+        this._snapshots[id] = {
+            date: date,
+            items: {}
+        };
+
         return {
-            id: "snap-aaa"
+            id: id
         };
     }
 
     acceptSnapshotItems(snapshotId, items)
     {
+        var snapshotInfo = this._snapshots[snapshotId];
+        if (!snapshotInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        for(var item of items)
+        {
+            snapshotInfo.items[item.hash] = item.data;
+        }
+
         return {};
     }
 
     activateSnapshot(snapshotId)
     {
+        var snapshotInfo = this._snapshots[snapshotId];
+        if (!snapshotInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        this._acceptSnapshot(snapshotInfo);
+
         return {};
     }
 
     newDiff(snapshotId, date)
     {
+        var snapshotInfo = this._snapshots[snapshotId];
+        if (!snapshotInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        var id = uuidv4();
+        this._diffs[id] = {
+            date: date,
+            snapshotId: snapshotId,
+            items: []
+        };
+
         return {
-            id: "diff-ccc"
+            id: id
         };
     }
 
     acceptDiffItems(diffId, items)
     {
+        var diffInfo = this._diffs[diffId];
+        if (!diffInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        for(var item of items)
+        {
+            diffInfo.items.push(item);
+        }
+
         return {};
     }
 
     activateDiff(diffId)
     {
+        var diffInfo = this._diffs[diffId];
+        if (!diffInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        var snapshotInfo = this._snapshots[diffInfo.snapshotId];
+        if (!snapshotInfo) {
+            return RESPONSE_NEED_NEW_SNAPSHOT;
+        }
+
+        var newSnapshotId = uuidv4();
+        var newSnapshotInfo = {
+            date: diffInfo.date,
+            items: _.clone(snapshotInfo.items)
+        };
+        this._snapshots[newSnapshotId] = newSnapshotInfo;
+
+        for(var diffItem of diffInfo.items)
+        {
+            if (diffItem.present)
+            {
+                newSnapshotInfo.items[diffItem.hash] = diffItem.data;
+            }
+            else
+            {
+                delete newSnapshotInfo.items[diffItem.hash];
+            }
+        }
+
+        delete this._snapshots[diffInfo.snapshotId];
+
+        this._acceptSnapshot(newSnapshotInfo);
+
         return {
-            id: "snap-bbb"
+            id: newSnapshotId
         };
     }
 
+    _acceptSnapshot(snapshotInfo)
+    {
+        this.logger.info("[_acceptSnapshot] item count: %s", _.keys(snapshotInfo.items).length);
+    }
+
 }
+
+const RESPONSE_NEED_NEW_SNAPSHOT = {
+    new_snapshot: true
+};
 
 module.exports = Collector;
