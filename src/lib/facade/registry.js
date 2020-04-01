091863d6332c12bfa2d9245879d7a23674fe6b19
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-04-01 14:48:40 -0700

    cleanup

--- src/lib/facade/registry.js
+++ src/lib/facade/registry.js
@@ -8,9 +8,6 @@ class FacadeRegistry
         this._context = context;
         this._logger = context.logger.sublogger("FacadeRegistry");
 
-        this._itemsMap = {};
-
-        this._logicTree = {};
         this._configMap = {};
     }
 
@@ -18,88 +15,17 @@ class FacadeRegistry
         return this._logger;
     }
 
-    get logicTree() {
-        return this._logicTree;
-    }
-
-    get configTree() {
-        return this._configMap;
-    }
-
-    updateLogicTree(value)
-    {
-        this._logger.info("[updateLogicTree] ...");
-        this._logicTree = value;
-    }
-    
-    updateConfigTree(value)
-    {
-        this._logger.info("[updateConfigTree] ...");
-        this._configMap = value;
-    }
-
-    acceptItems(value)
-    {
-        this._logger.info("[acceptItems] ...");
-        this._itemsMap = value;
-
-        this._context.historyProcessor.acceptSnapshot(value);
-
-        this._context.searchEngine.reset();
-        for(var item of _.values(this._itemsMap))
-        {
-            this._context.searchEngine.addToIndex(item);
-        }
-    }
-
-    getConfig(dn) {
-        var value = this._configMap[dn];
-        if (!value) {
-            return {};
-        }
-        return value;
-    }
-
-    getItem(dn) {
-        var value = this._itemsMap[dn];
-        if (!value) {
-            return {};
-        }
-        return {
-            dn: value.dn
-        };
-    }
-    
-    getItemChildren(dn) {
-        var value = this._itemsMap[dn];
-        if (!value) {
-            return [];
-        }
-        return value.getChildren().map(x => ({
-            dn: x.dn,
-            order: x.order
-        }));
-    }
-
-    getItemProperties(dn) {
-        var value = this._itemsMap[dn];
-        if (!value) {
-            return [];
-        }
-        return value.extractProperties();
-    }
-
-    getItemAlerts(dn) {
-        var value = this._itemsMap[dn];
-        if (!value) {
-            return [];
-        }
-        return value.extractAlerts();
-    }
-
     acceptCurrentSnapshot(snapshotInfo)
     {
         this._context.registry.accept(snapshotInfo);
+
+        //     this._context.historyProcessor.acceptSnapshot(value);
+
+        //     this._context.searchEngine.reset();
+        //     for(var item of _.values(this._itemsMap))
+        //     {
+        //         this._context.searchEngine.addToIndex(item);
+        //     }
     }
 }
 
