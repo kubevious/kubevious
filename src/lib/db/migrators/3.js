7c47004a8cb17d03f284598f8edb450c0397f6b0
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-05-15 18:07:58 -0700

    added rule tables migrators

--- src/lib/db/migrators/3.js
+++ src/lib/db/migrators/3.js
@@ -2,6 +2,7 @@ module.exports = function(logger, driver) {
     logger.info("MIGRATING v3");
 
     var sql = "" + 
+
     "CREATE TABLE IF NOT EXISTS `rules` (" + 
         "`id` int unsigned NOT NULL AUTO_INCREMENT," + 
         "`name` varchar(128) NOT NULL," + 
@@ -10,6 +11,28 @@ module.exports = function(logger, driver) {
         "`script` TEXT NOT NULL," + 
         "PRIMARY KEY (`id`)" + 
     ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+
+    + 
+
+    "CREATE TABLE IF NOT EXISTS `rule_logs` (" +
+        "`id` int unsigned NOT NULL AUTO_INCREMENT," +
+        "`name` varchar(128) NOT NULL," +
+        "`kind` varchar(128) NOT NULL," +
+        "`msg` TEXT NOT NULL," +
+        "PRIMARY KEY (`id`)" + 
+    ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+
+    +
+
+    "CREATE TABLE IF NOT EXISTS `rule_items` (" +
+        "`id` int unsigned NOT NULL AUTO_INCREMENT," +
+        "`name` varchar(128) NOT NULL," +
+        "`dn` varchar(1024) NOT NULL," + 
+        "`has_error` TINYINT," +
+        "`has_warning` TINYINT," +
+        "PRIMARY KEY (`id`)" + 
+    ") ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+
     ;
 
     return driver.executeSql(sql);
