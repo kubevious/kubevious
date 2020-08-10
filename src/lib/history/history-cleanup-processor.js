602483fdd061debb572cd93394ed6418f24cd88b
Semyon Pukhov <semyonpuchof@gmail.com>
2020-08-10 11:08:41 +0300

    Improvements in db cleanup

--- src/lib/history/history-cleanup-processor.js
+++ src/lib/history/history-cleanup-processor.js
@@ -1,5 +1,6 @@
 const _ = require('the-lodash');
 const moment = require('moment')
+const Promise = require('the-promise')
 
 class HistoryCleanupProcessor {
     constructor(context)
@@ -47,10 +48,8 @@ class HistoryCleanupProcessor {
         return Promise.resolve()
             .then(() => this.fetchSnapshot(snapDate))
             .then(snapshot => {
-                return Promise.resolve(snapshot.map(item => this.fetchDiffs(item.id)))
-                    .then(() => {
-                        return this.deleteSnapshot(snapshot[0].id)
-                    })
+                return Promise.serial(snapshot, item => this.deleteDiffs(item.id))
+                    .then(() => this.deleteSnapshot(snapshot[0].id))
             })
     }
 
@@ -59,14 +58,14 @@ class HistoryCleanupProcessor {
         return this._execute('FIND_OLDEST_SNAPSHOT_ID', date)
     }
 
-    fetchDiffs(snapshotId)
+    deleteDiffs(snapshotId)
     {
         return this._execute('FIND_DIFFS_FOR_SNAPSHOT', [snapshotId])
             .then(diffs => {
-                return Promise.resolve(diffs.map(item => {
-                    return this.deleteDiffItems(item.id)
-                        .then(() => this._execute('DELETE_DIFFS_FOR_SNAPSHOT', [item.id]))
-                }))
+                return Promise.serial(diffs, diff => {
+                    return this.deleteDiffItems(diff.id)
+                        .then(() => this._execute('DELETE_DIFFS_FOR_SNAPSHOT', [snapshotId]))
+                })
             })
             .then(() => {
                 return this.deleteSnapshotItem(snapshotId)
@@ -79,9 +78,9 @@ class HistoryCleanupProcessor {
             .then(res => res)
     }
 
-    deleteSnapshotItem(snapshotIds)
+    deleteSnapshotItem(snapshotId)
     {
-        return this._execute('DELETE_SNAP_ITEMS_BY_SNAPSHOT_ID', [snapshotIds])
+        return this._execute('DELETE_SNAP_ITEMS_BY_SNAPSHOT_ID', [snapshotId])
             .then(res => res)
     }
 
