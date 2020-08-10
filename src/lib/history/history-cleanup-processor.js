3d842ce5104990e92344326784b72447fd5bf2ed
Semyon Pukhov <semyonpuchof@gmail.com>
2020-08-10 15:25:04 +0300

    Implemented config_hashes deleting

--- src/lib/history/history-cleanup-processor.js
+++ src/lib/history/history-cleanup-processor.js
@@ -8,6 +8,7 @@ class HistoryCleanupProcessor {
         this._context = context;
         this._logger = context.logger.sublogger('HistoryCleanupProcessor');
         this._database = context.database;
+        this._configHashesDict = {}
         this._days = 14
 
         context.database.onConnect(this._onDbConnected.bind(this));
@@ -23,28 +24,47 @@ class HistoryCleanupProcessor {
 
     _registerStatements()
     {
+        this._registerStatement('FIND_CONFIG_HASHES', 'SELECT `key` FROM `config_hashes`')
+
         this._registerStatement('FIND_OLDEST_SNAPSHOTS', 'SELECT `id` FROM `snapshots` WHERE `date` < ? ORDER BY `date`')
 
         this._registerStatement('FIND_DIFFS_FOR_SNAPSHOT', 'SELECT `id` FROM `diffs` WHERE `snapshot_id` = ?')
+
+        this._registerStatement('FIND_DIFF_ITEMS_CONFIG_HASHES', 'SELECT `config_hash` FROM `diff_items` WHERE `diff_id` = ?')
 
         this._registerStatement('DELETE_DIFF_ITEMS_FOR_DIFF', 'DELETE FROM `diff_items` WHERE `diff_id` = ?')
 
         this._registerStatement('DELETE_DIFFS_FOR_SNAPSHOT', 'DELETE FROM `diffs` WHERE `snapshot_id` = ?')
 
+        this._registerStatement('FIND_SNAP_ITEMS_CONFIG_HASHES', 'SELECT `config_hash` FROM `snap_items` WHERE `snapshot_id` = ?')
+
         this._registerStatement('DELETE_SNAP_ITEMS_BY_SNAPSHOT_ID', 'DELETE FROM `snap_items` WHERE `snapshot_id` = ?')
 
         this._registerStatement('DELETE_SNAPSHOT_BY_ID', 'DELETE FROM `snapshots` WHERE `id` = ?')
+
+        this._registerStatement('DELETE_CONFIG_HASHES', 'DELETE FROM `config_hashes` WHERE `key` = ?')
     }
 
     _retryToDelete()
     {
-        const snapDate = [moment().subtract(this._days, 'days').format()];
+        return this._queryConfigHashes()
+            .then(() => {
+                const snapDate = [moment().subtract(this._days, 'days').format()];
 
-        return this.fetchAllSnapshots(snapDate)
-            .then(snapshots => {
-                return Promise.serial(snapshots, snapshot => this.cleanDb(snapshot))
-            }).catch(() => {
-                this._logger.error('No more snapshots')
+                return this.fetchAllSnapshots(snapDate)
+                    .then(snapshots => {
+                        return Promise.serial(snapshots, snapshot => this.cleanDb(snapshot))
+                    }).catch(() => {
+                        this._logger.error('No more snapshots')
+                    })
+            })
+    }
+
+    _queryConfigHashes()
+    {
+        return this._execute('FIND_CONFIG_HASHES')
+            .then(hashes => {
+                return this._configHashesDict = _.makeDict(hashes, x => x.key)
             })
     }
 
@@ -52,6 +72,7 @@ class HistoryCleanupProcessor {
     {
         return this.deleteDiffs(snapshot.id)
             .then(() => this.deleteSnapshot(snapshot.id))
+            .then(() => this.deleteConfigHashes())
     }
 
     fetchAllSnapshots(date)
@@ -63,32 +84,54 @@ class HistoryCleanupProcessor {
     {
         return this._execute('FIND_DIFFS_FOR_SNAPSHOT', [snapshotId])
             .then(diffs => {
-                return Promise.serial(diffs, diff => {
-                    return this.deleteDiffItems(diff.id)
-                        .then(() => this._execute('DELETE_DIFFS_FOR_SNAPSHOT', [snapshotId]))
-                })
+                return Promise.serial(diffs, diff => this.deleteDiffItems(diff.id))
+                    .then(() => this._execute('DELETE_DIFFS_FOR_SNAPSHOT', [snapshotId]))
             })
-            .then(() => {
-                return this.deleteSnapshotItem(snapshotId)
-            })
+            .then(() => this.deleteSnapshotItem(snapshotId))
     }
 
     deleteDiffItems(diffId)
     {
-        return this._execute('DELETE_DIFF_ITEMS_FOR_DIFF', [diffId])
-            .then(res => res)
+        return this._execute('FIND_DIFF_ITEMS_CONFIG_HASHES', [diffId])
+            .then(hashes => {
+                this._cleanConfigHashes(hashes)
+
+                return this._execute('DELETE_DIFF_ITEMS_FOR_DIFF', [diffId])
+                    .then(res => res)
+            })
     }
 
     deleteSnapshotItem(snapshotId)
     {
-        return this._execute('DELETE_SNAP_ITEMS_BY_SNAPSHOT_ID', [snapshotId])
-            .then(res => res)
+        return this._execute('FIND_SNAP_ITEMS_CONFIG_HASHES', [snapshotId])
+            .then(hashes => {
+                this._cleanConfigHashes(hashes)
+            })
+            .then(() => {
+                return this._execute('DELETE_SNAP_ITEMS_BY_SNAPSHOT_ID', [snapshotId])
+                    .then(res => res)
+            })
+
     }
 
     deleteSnapshot(snapshotId)
     {
         return this._execute('DELETE_SNAPSHOT_BY_ID', [snapshotId])
             .then(res => res)
+    }
+
+    deleteConfigHashes()
+    {
+        const hashes = Object.keys(this._configHashesDict)
+        return Promise.serial(hashes, hash =>
+            this._execute('DELETE_CONFIG_HASHES', [hash.key]))
+    }
+
+    _cleanConfigHashes(hashes)
+    {
+        hashes.forEach(hash => {
+            delete this._configHashesDict[hash.config_hash]
+        })
     }
 
     _registerStatement(name, sql)
